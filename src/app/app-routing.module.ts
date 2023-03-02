e912ffe1f29b2c29474f72af36569d3df38ca868
Bociarz1 <mikrofoncrk@gmail.com>
2023-03-02 22:22:38 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/page/login/login.component';
+import { RegisterComponent } from './modules/auth/page/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: LoginComponent},
+  {path:"login", component: LoginComponent},
+  {path:"register", component: RegisterComponent},
+  // {path:"**", component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent,RegisterComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import { ChipModule } from 'primeng/chip';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ChipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
